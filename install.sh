123dfb4ae8421a0f749818a4a5bbb6389b151ac3
Randy1burrell <randy1burrell@yahoo.com>
2017-08-11 16:53:09 -0500

    installation bash script created

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+############################################################
+## Author: Randy Burrell                                  ##
+## Date Created: 2017/11/08                               ##
+##                                                        ##
+## Description: This file is used to install a            ##
+##              development environment for the linux     ##
+##              terminal which includes: zsh shell, tmux, ##
+##              tmuxinator, rvm, omyzh. It also changes   ##
+##              vi's/vim's, tmux and zsh confiuration to  ##
+##              match the config included with this       ##
+##              repository.                               ##
+##                                                        ##
+## Usage: Change mode of file to be executable and run as ##
+##        root.                                           ##
+############################################################
