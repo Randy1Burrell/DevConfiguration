4b9cba9b730432594c188fbf756eae8070c9e3fc
Randy1burrell <randy1burrell@yahoo.com>
2017-08-12 18:53:35 -0500

    Created functions for main menu options

--- install.sh
+++ install.sh
@@ -22,3 +22,43 @@ install_vim()
 {
   apt install vim
 }
+
+main_menu()
+{
+    while :
+    do
+        clear
+        echo "Welcome to development configuration's interactive shell"
+        echo "Please select from the options below"
+        echo "1 -- install all configuration"
+        echo "2 -- install and configure vim"
+        echo "3 -- install and configure zsh"
+        echo "4 -- install and configure tmux"
+        echo "5 -- Quit"
+        read main_menu_option
+
+        case $main_menu_option in
+            1)
+                configure_all
+                ;;
+            2)
+                vim_menu
+                ;;
+            3)
+                zsh_menu
+                ;;
+            4)
+                tmux_menu
+                ;;
+            5)
+                return 0
+        esac
+    done
+}
+
+main()
+{
+    main_menu
+}
+
+main
