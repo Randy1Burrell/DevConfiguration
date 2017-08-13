e4a5157f88dc393064316995ff63fa3c162712cc
Randy1burrell <randy1burrell@yahoo.com>
2017-08-13 16:04:39 -0500

    Refactor, new features and comments

--- scripts/vim/vim_functions.sh
+++ scripts/vim/vim_functions.sh
@@ -9,30 +9,122 @@
 ##         script and call any function defined
 ##         below.
 
+## Generic error function
+vim_error()
+{
+    case $1 in
+        0)
+            echo ".vimrc has been replaced!!!"
+            sleep 1
+            return
+            ;;
+        1)
+            echo "New .vimrc file was not supplied"
+            sleep 1
+            return
+            ;;
+        2)
+            echo "Something went wrong!!!"
+            echo "Please ensure that the configs folder is present"
+            echo "Also ensure configs folder contains a vim folder"
+            echo "And vim folder includes a vimrc file within it"
+            sleep 10
+            return
+            ;;
+        3)
+            echo "Invalid action!!!"
+            sleep 1
+            return
+            ;;
+        4)
+            echo "No proper action supplied"
+            sleep 1
+            return
+            ;;
+        5)
+            echo "Files have been merged!! :-)"
+            sleep 1
+            return
+            ;;
+        6)
+            echo "File appended!!! :-)"
+            sleep 1
+            return
+            ;;
+        *)
+            echo "Something went wrong!!!"
+            echo "Wait a minute then try again"
+            sleep 2
+            return
+            ;;
+    esac
+}
+
+## Name: generic_vim_actions
+## Desc: Does an action based on parameters passed
+## Param: string -- path to new vimrc
+##        integer -- what action to perform
+##        1 -- replace vimrc
+##        2 -- merge vimrc
+##        3 -- append vimrc
+## Return: integer
+generic_vim_actions()
+{
+    if [ "$#" -lt "2" ]
+    then
+        ## Enough arguments not supplied
+        return 1
+    fi
+    if [ ! -f "$1" ]
+    then
+        ## File not found
+        return 2
+    fi
+    if [ "$2" -gt "3" ]
+    then
+        ## Action is invalid
+        return 3
+    fi
+    ## Define user's vim configuration file
+    vimrc="${HOME}//.vimrc"
+    case $2 in
+        1)
+            ## Replace contents on current user's vim config file
+            cat $1 > $vimrc
+            return 0
+            ;;
+        2)
+            dir=`pwd` ## Get the present working directory
+            mkdir "$dir//tmp" ## Define a temporary directory here
+            conf="$dir//tmp//vimrc.diff" ## Define temporary confile path
+            ## Write difference between our vimrc file and present user's confile to temporary file
+            diff $1 "$vimrc" | grep "<" | sed 's/^<//g' > "$conf"
+            ## Append the difference to current users vimrc
+            cat $conf >> $vimrc
+            ## Removing tmp directory
+            rm -R "$dir//tmp"
+            return 5
+            ;;
+        3)
+            ## Append contents of our vimrc file to current user's
+            cat $1 >> $vimrc
+            return 6
+            ;;
+        *)
+            return 4
+            ;;
+    esac
+}
+
 ## Name: vim_replace
 ## Desc: replaces .vimrc with the one given
 ## Param: string -- path to new vimrc
 ## Return: integer
 vim_replace()
 {
-    if [ $# -lt 1 ]
-    then
-        echo "Usage:"
-        echo "vim_replace {path to .vimrc to replace with old .vimrc with}"
-        sleep 3
-        return 1
-    fi
-
-    if [ ! -f $1 ]
-    then
-        echo "File supplied does not exists"
-        echo "Please give path to a real file"
-        sleep 2
-        return 2
-    else
-        cat $1 > "${HOME}//.vimrc"
-    fi
-    return 0
+    ## Call generic vim actions and return its return value
+    generic_vim_actions $1 1
+    return $?
 }
 
 ## Name: vim_append
@@ -41,7 +133,8 @@ vim_replace()
 ## Return: integer
 vim_append()
 {
-    return 0
+    ## Call generic vim actions and return its return value
+    return generic_vim_actions $1 2
 }
 
 ## Name: select_vim_install
@@ -51,6 +144,7 @@ vim_append()
 ## Return: integer
 select_vim_install()
 {
+    ## Does nothing yet
     return 0
 }
 
