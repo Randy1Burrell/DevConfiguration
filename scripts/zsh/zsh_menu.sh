c8345cd384ffc98a0e9836b47756fbc243f8a435
Randy1burrell <randy1burrell@yahoo.com>
2017-08-13 17:17:46 -0500

    Adding zsh menu

--- scripts/zsh/zsh_menu.sh
+++ scripts/zsh/zsh_menu.sh
@@ -6,4 +6,59 @@
 ## Interactive menu to configure zsh
 zsh_menu()
 {
+    ## Loop keeps user at menu until the select
+    ## an option in the menu
+    while :
+    do
+        clear
+        echo "*******************************"
+        echo "ZSH MENU OPTIONS"
+        echo "Select an option"
+        echo "1 -- replace your .zshrc with ours"
+        echo "2 -- add our .zshrc to yours"
+        echo "3 -- install and configure zsh"
+        echo "4 -- install and configure OH_MY_ZSH"
+        echo "5 -- return to previous menu"
+        echo "6 -- Quit"
+        read vim_option
+
+        case $vim_option in
+            1)
+                clear
+                echo "************* WARNING!!!!! *******************"
+                echo "## This could be dangerous"
+                echo "## Your .zshrc file may have settings that is"
+                echo "## Specific to your environment"
+                echo "## Select:"
+                echo "## [y]--YES"
+                echo "## [ANY OTHER KEY]--NO"
+                echo "**********************************************"
+                read opt
+                    if [ $opt = "y" ] || [ $opt = "Y" ]
+                    then
+                        curr_dir=`pwd`
+                        conf_file="${curr_dir}//configs//zsh//zshrc"
+                    fi
+                ;;
+            2)
+                curr_dir=`pwd`
+                conf_file="${curr_dir}//configs//vim//vimrc"
+                ;;
+            3)
+                ;;
+            4)
+                ;;
+            5)
+                return 0
+                ;;
+            6)
+                clear
+                exit
+                ;;
+            *)
+                echo "Options are 1-6"
+                sleep 1
+                ;;
+        esac
+    done
 }
