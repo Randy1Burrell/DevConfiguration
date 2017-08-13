8529b09f598a2d55973e45098bc12c677147eff4
Randy1burrell <randy1burrell@yahoo.com>
2017-08-13 16:25:54 -0500

    Lib file for zsh menu

---
+++ scripts/zsh/zsh_menu.sh
@@ -0,0 +1,6 @@
+## Zsh Menu File
+## This file should not be called independently
+## To be imported and used in another program
+## zsh menu function displays
+
+## Interactive menu to configure zsh
