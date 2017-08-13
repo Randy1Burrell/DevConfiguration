1ac0593ad02eac6b9268e25bc241ab59b46b3a43
Randy1burrell <randy1burrell@yahoo.com>
2017-08-12 22:59:19 -0500

    Added file which will contain function to be used to configure vim

---
+++ scripts/vim/vim_functions.sh
@@ -0,0 +1,10 @@
+## Author: Randy Burrell
+## Date Created: 2017/08/12
+##
+## @desc: this file contains functions that can
+##        be used to help configure vim.
+##
+## @usage: To call any function within this file
+##         import this script in your main shell
+##         script and call any function defined
+##         below.
