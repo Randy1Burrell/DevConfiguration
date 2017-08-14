8fa464ad15ed42868f9aeea7a861a2a30fb0f45e
Randy1burrell <randy1burrell@yahoo.com>
2017-08-13 19:42:42 -0500

    Alias file for zsh

---
+++ configs/zsh/alias/alias.zsh
@@ -0,0 +1,19 @@
+# aliases for Tmux
+alias tmux='tmux -2'
+alias ta='tmux attach -t'
+alias tnew='tmux new -s'
+alias tls='tmux ls'
+alias tkill='tmux kill-session -t'
+alias tkills='tmux kill-session'
+alias td='tmux detach'
+alias tdt='tmux detach -t'
+
+# convenience aliases for editing configs
+alias ev='vim ~/.vimrc'
+alias et='vim ~/.tmux.conf'
+alias ez='vim ~/.zshrc'
+
+# convenient aliases for updating prompt
+alias sv='source ~/.vimrc'
+alias sz='source ~/.zshrc'
+
