cc06f7214174e7a072235dc6bc5a99b9798a1a3c
Randy1burrell <randy1burrell@yahoo.com>
2017-08-14 22:29:29 -0500

    Some more aliases for tmuxinator

--- configs/zsh/alias/alias.zsh
+++ configs/zsh/alias/alias.zsh
@@ -3,17 +3,22 @@ alias tmux='tmux -2'
 alias ta='tmux attach -t'
 alias tnew='tmux new -s'
 alias tls='tmux ls'
-alias tkill='tmux kill-session -t'
-alias tkills='tmux kill-session'
 alias td='tmux detach'
 alias tdt='tmux detach -t'
+alias tkill='tmux kill-session -t'
+alias tkills='tmux kill-session'
+alias tm='tmuxinator'
+alias tms='tmuxinator start'
+alias tml='tmuxinator list'
+alias tme='tmuxinator edit'
+alias tmc='tmuxinator copy'
 
 # convenience aliases for editing configs
 alias ev='vim ~/.vimrc'
+alias ea='vim ~/.zsh/alias.zsh'
 alias et='vim ~/.tmux.conf'
 alias ez='vim ~/.zshrc'
 
 # convenient aliases for updating prompt
-alias sv='source ~/.vimrc'
 alias sz='source ~/.zshrc'
 
