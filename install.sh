404ffac412414d3fe2d3cf8202c48cf3c6020afa
Randy1burrell <randy1burrell@yahoo.com>
2017-08-16 01:12:47 -0500

    Factored generic functions to scripts/generic/ggeneric_functions.sh

--- install.sh
+++ install.sh
@@ -21,52 +21,7 @@
 . ./scripts/zsh/zsh_functions.sh
 . ./scripts/zsh/zsh_menu.sh
 . ./scripts/tmux/tmux_functions.sh
-
-is_package_installed()
-{
-    dpkg -s $1 &> /dev/null
-
-    if [ $? -eq 0 ]; then
-      return 0
-    else
-      return 1
-    fi
-}
-
-install()
-{
-    if [ `is_package_installed $1` -eq 1 ]
-    then
-      sudo apt install $1
-    else
-      echo "Will not install $1"
-      echo "$1 is already installed"
-    fi
-}
-
-init_var()
-{
-    dir=`pwd`
-    vimrc="${dir}//configs//vim//vimrc"
-    zsh_conf_file="${dir}//configs//zsh//zshrc"
-}
-
-configure_all()
-{
-    ## Get vundle
-    get_vim_vundle
-    ## Get pathogen
-    get_vim_pathogen
-    ## Get vim colors
-    do_colors
-    ## Replace .vimrc for curren user
-    vim_replace $vimrc
-    vim_op_mesg $?
-    ## Configure zsh for current user
-    zsh_config $zsh_conf_file
-    ## Install tmux
-    tmux_install
-}
+. ./scripts/generic/generic_functions.sh
 
 main_menu()
 {
--- scripts/generic/generic_functions.sh
+++ scripts/generic/generic_functions.sh
@@ -8,3 +8,51 @@
 ## Usage: To call any function within this file
 ##         import this script in your main shell
 ##         script and call any function defined
+
+is_package_installed()
+{
+    dpkg -s $1 &> /dev/null
+
+    if [ $? -eq 0 ]; then
+      return 0
+    else
+      return 1
+    fi
+}
+
+install()
+{
+    if [ `is_package_installed $1` -eq 1 ]
+    then
+      sudo apt install $1
+    else
+      echo "Will not install $1"
+      echo "$1 is already installed"
+    fi
+}
+
+init_var()
+{
+    dir=`pwd`
+    vimrc="${dir}//configs//vim//vimrc"
+    zsh_conf_file="${dir}//configs//zsh//zshrc"
+}
+
+configure_all()
+{
+    ## Get vundle
+    get_vim_vundle
+    ## Get pathogen
+    get_vim_pathogen
+    ## Get vim colors
+    do_colors
+    ## Replace .vimrc for curren user
+    vim_replace $vimrc
+    vim_op_mesg $?
+    ## Configure zsh for current user
+    zsh_config $zsh_conf_file
+    ## Install tmux
+    tmux_install
+}
+
+
