9e5040186e36f265ded6bd0e88ab006d8c6f23e7
Randy1burrell <randy1burrell@yahoo.com>
2017-08-16 00:57:21 -0500

    File for tmux configuration functions

---
+++ scripts/tmux/tmux_functions.sh
@@ -0,0 +1,21 @@
+## Author: Randy Burrell
+## Date Created: 2017/08/16
+##
+## Desc: this file contains functions that can
+##        be used to help configure tmux and
+##        tmuxinator.
+##
+## Usage: To call any function within this file
+##         import this script in your main shell
+##         script and call any function defined
+##         below.
+
+## Name: tmux_install
+## Description: installs tmux if it has not been
+##              already been installed
+## Params: ---
+## Return: ---
+tmux_install()
+{
+  install tmux
+}
