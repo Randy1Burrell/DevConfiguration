895bd722e980a8a5df997d0dbdaa8e9396fcd16d
Randy1burrell <randy1burrell@yahoo.com>
2017-08-16 01:08:41 -0500

    Created generic functions file. See file header for useand description

---
+++ scripts/generic/generic_functions.sh
@@ -0,0 +1,10 @@
+## Author: Randy Burrell
+## Date Created: 2017/08/16
+##
+## Desc: this file contains generic functions
+##       used as helper functions in other
+##       areas of this program
+##
+## Usage: To call any function within this file
+##         import this script in your main shell
+##         script and call any function defined
