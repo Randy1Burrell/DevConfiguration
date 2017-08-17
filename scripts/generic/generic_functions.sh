c782b6fcab901937ec7f3766ab7f0aa075ce2e97
Randy1burrell <randy1burrell@yahoo.com>
2017-08-16 19:31:28 -0500

    Generic function used to manage rvm

--- scripts/generic/generic_functions.sh
+++ scripts/generic/generic_functions.sh
@@ -50,6 +50,79 @@ init_var()
     zsh_conf_file="${dir}//configs//zsh//zshrc"
 }
 
+## Name: check_ppa
+## Desc: Checks if ppa has already been added to the
+##       source list
+## Params: string -- ppa name
+## return: integer - 0 on success
+check_ppa()
+{
+  grep $1 /etc/apt/sources.list /etc/apt/sources.list.d/*
+  return $?
+}
+
+## Name: rvm_gpg
+## Desc: Adds rvm gpg key and install rvm
+## Params: ---
+## Returns: ---
+rvm_gpg()
+{
+  ## Check if gpg keys already exist and add them
+  ## if they don't
+  `gpg -k 409B6B1796C275462A1703113804BB82D39DC0E3`
+  res=$?
+  if [ res -ne 0 ]; then
+    `gpg -k 7D2BAF1CF37B13E2069D6956105BD0E739499BDB`
+    res=$?
+    if [ res -ne 0 ]; then
+      gpg --keyserver hkp://keys.gnupg.net \
+      --recv-keys \
+      409B6B1796C275462A1703113804BB82D39DC0E3 \
+      7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+    fi
+  fi
+  ## Check if rvm is already installed
+  `rvm`
+  res=$?
+  if [ res -ne 0 ]; then
+    \curl -sSL https://get.rvm.io | bash -s stable --ruby
+  fi
+}
+
+## Name: rvm_manager
+## Desc: Manages rvm installation process depending
+##       on OS type. Example if OS is ubuntu use
+##       apt package manager
+## Params: ---
+## Return: ---
+rvm_manager()
+{
+  ## If OS is Ubuntu the install rvm from ppa
+  ## else install rvm for other OSes
+  `uname -a | grep -i ubuntu`
+  res=$?
+  if [ res -eq 0 ]; then
+    ## If software-properties-common has not been
+    ## installed; then install it
+    res=is_package_installed "software-properties-common"
+    if [ res -ne 0 ]; then
+      install software-properties-common
+    fi
+    ## If ppa:rael-gc/rvm was not added; add it
+    if [ check_ppa -ne 0 ]; then
+      sudo apt-add-repository -y ppa:rael-gc/rvm
+      sudo apt update
+    fi
+    ## If rvm is not installed; install it
+    res=is_package_installed "rvm"
+    if [ res -ne 0 ]; then
+      install rvm
+    fi
+  else
+    rvm_gpg
+  fi
+}
+
 ## Name: Configure_all
 ## Desc: Completely comfigures you dev env
 ## Params: ---
