512952ef45e1efefb47f06fbd9d77a77a64750a0
Randy1burrell <randy1burrell@yahoo.com>
2017-08-28 22:53:24 -0500

    Modified tmux_install() to interactively help the user configure tmux

--- scripts/tmux/tmux_functions.sh
+++ scripts/tmux/tmux_functions.sh
@@ -17,7 +17,24 @@
 ## Return: ---
 tmux_install()
 {
-  install tmux
+  ## Confirm that the user want to install tmux
+  if (whiptail --title "Confirmation" --yesno "Are you sure you would like to install $res?" 8 60) then
+    ## Get the user's password
+    password=$(whiptail --title "Password Dialog" --passwordbox "Please enter your correct password" 10 60 \
+        3>&1 1>&2 2>&3)
+    res="$?"
+    if [ $res -eq 0 ]; then
+      install "tmux"
+      res="$?"
+    else
+      return $res
+    fi
+  fi
+  if [ $res -eq 0 ]; then
+    if (whiptail --title "Confirmation" --yesno "tmux has been successfully installed\nWould you like to configure you tmux.conf" 8 60) then
+      replace_conf
+    fi
+  fi
 }
 
 ## Name: config_tmuxinator
@@ -29,15 +46,15 @@ config_tmuxinator()
   gem install tmuxinator
 }
 
-## Name: replace_tmux_conf
+## Name: replace_conf
 ## Desc: Replaces current user's .tmux.conf with conf
 ##       in configs/tmux/tmux.conf
 ## Params: ---
 ## Return: ---
-replace_tmux_conf()
+replace_conf()
 {
-  cat "${dir}//configs//tmux//tmux.conf" \
-  > "${HOME}//.tmux.conf"
+  tm_conf="${HOME}//.tmux.conf"
+  cat "$tmux_conf" > "$tm_conf"
 }
 
 ## Name: tmux_config_all
